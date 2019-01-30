79c7299f7059147cb5d59ab48414abaf3bad45e4
clifford <clifford@noukod.com>
2019-01-30 21:58:40 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 100 }
-  #validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  #validate :is_clickbait?
-
 end
